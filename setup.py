859e6727667b02175e3312f50f32c5e4a8a49ffe
h0nda <41300123+h0nde@users.noreply.github.com>
2021-05-13 10:20:19 +0200

    move

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="xrequests",
+    author="h0nda",
+    author_email="1@1.com",
+    description="A lazy, but fast requests module",
+    url="https://github.com/h0nde/xrequests",
+    packages=setuptools.find_packages(),
+    classifiers=[]
+)
