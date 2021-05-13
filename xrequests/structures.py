cc56d1e480faa861f0105efe9e7e369c379b47e4
h0nda <41300123+h0nde@users.noreply.github.com>
2021-05-13 11:09:02 +0200

    Update structures.py

--- xrequests/structures.py
+++ xrequests/structures.py
@@ -1,6 +1,7 @@
 from collections import OrderedDict
 from collections.abc import Mapping, MutableMapping
 
+# copied from psf/requests
 class CaseInsensitiveDict(MutableMapping):
     """A case-insensitive ``dict``-like object.
     Implements all methods and operations of
