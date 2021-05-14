137e48c8a10a79279ae07cdb04a793619e2b2f8b
h0nda <41300123+h0nde@users.noreply.github.com>
2021-05-14 10:33:02 +0200

    Update models.py

--- xrequests/models.py
+++ xrequests/models.py
@@ -1,6 +1,15 @@
+import json as jsonlib
+
 class Response:
     def __init__(self, status, message, headers, content):
         self.status = status
         self.message = message
         self.headers = headers
         self.content = content
+    
+    def json(self):
+        return jsonload.load(self.content)
+    
+    @property
+    def text(self):
+        return self.content.decode("UTF-8", errors="ignore")
