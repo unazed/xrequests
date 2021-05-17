dc31606a353bdd4560e2c81766e4d53d8737bd83
h0nda <41300123+h0nde@users.noreply.github.com>
2021-05-17 14:00:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ with open("requirements.txt") as fp:
 setuptools.setup(
     name="xrequests",
     author="h0nda",
-    author_email="1@1.com",
     description="A faster alternate for the requests library",
     url="https://github.com/h0nde/xrequests",
     packages=setuptools.find_packages(),
