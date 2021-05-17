92fd9a43df5cdcbc0101e050c8d3673a731629b2
h0nda <41300123+h0nde@users.noreply.github.com>
2021-05-17 14:00:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="xrequests",
     author="h0nda",
     author_email="1@1.com",
-    description="A lazy, but fast requests module",
+    description="A faster alternate for the requests library",
     url="https://github.com/h0nde/xrequests",
     packages=setuptools.find_packages(),
     classifiers=[],
